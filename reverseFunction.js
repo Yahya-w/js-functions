a23707fbb1698bd21ede71c292023850446bf01f
Yahya Altıntop <69236857+Yahya-w@users.noreply.github.com>
2021-07-06 09:01:30 +0300

    Update reverseFunction.js
    Code fix

--- reverseFunction.js
+++ reverseFunction.js
@@ -1,16 +1,17 @@
-    function myReverse(param) {
-        if (Array.isArray(param)) {
-            var _tmp;
-            var pLength = param.length
-            for (let i = 0; i < param.length; i++) {
-                if (i >= pLength) break;
-                _tmp = param[i]
-                param[i] = param[pLength - 1]
-                param[pLength - 1] = _tmp
-                pLength--
-            }
-            return param
-        } else {
-            throw TypeError(param.toString() + " is not an Array !")
+function myReverse(param) {
+    if (Array.isArray(param)) {
+
+        let pLength = param.length;
+
+        for (let i = 0; i < param.length; i++) {
+            if (i >= pLength) break;
+
+            [param[i], param[pLength - 1]] = [param[pLength - 1], param[i]];
+
+            pLength--;
         }
+        return param;
+    } else {
+        throw TypeError(`${param} is not an Array ! `);
     }
+}
